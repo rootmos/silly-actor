3a45a8176c73c99def570d6097210dd29a3095d9
Gustav Behm <gustav.behm@gmail.com>
2018-01-30 22:46:06 +0100

    Flesh out the Lsrc language a bit

--- silly-actor.scm
+++ silly-actor.scm
@@ -1,27 +1,57 @@
 (import (matchable) (nanopass))
 
-(define verbs '(match stop spawn send become))
+(define verbs '(match stop spawn send become actor stay self))
 
-(define (atom? x) (and (symbol? x) (not (member x verbs))))
-(define (verb? x) (list? (member x '(match stop spawn send become))))
+(define (verb? x) (list? (member x verbs)))
+(define (atom? x)
+  (and
+    [symbol? x]
+    [not (verb? x)]
+    [not (equal? (substring (symbol->string x) 0 1) "_")]
+    ))
 (define (variable-pattern? x) (match x [('unquote s) (atom? s)] [else #f]))
+(define (wildcard-pattern? x)
+  (and
+    [symbol? x]
+    [equal? (substring (symbol->string x) 0 1) "_"]
+    ))
 
 (define-language
   Lsrc
   (terminals
     (atom (a))
-    (verb (v))
     (number (n))
-    (variable-pattern (vp)))
+    (variable-pattern (vp))
+    (wildcard-pattern (wp)))
   (entry System)
   (Pattern (p)
-    vp
     a
     n
+    vp
+    wp
+    '()
     (p* ...))
+  (Expr (e)
+    a
+    n
+    ua
+    (become e0 e1)
+    (stay e)
+    (spawn e0 e1)
+    (send e0 e1)
+    (stop)
+    (self)
+    '()
+    (e* ...))
+  (MsgCase (m)
+    (p e))
+  (UnnamedActor (ua)
+    (actor (a) m* ...))
   (ActorDef (ad)
-    (actor a p))
+    (define (a p) m* ...))
+  (Options (o)
+    (start a))
   (System (t)
-    (system a ad* ...)))
+    (system (o* ...) ad* ...)))
 
 (define-parser parse-Lsrc Lsrc)
--- snippet.scm
+++ snippet.scm
@@ -1,15 +1,33 @@
 (load "silly-actor.scm")
 
 (pretty-print
-  [list
+  (and
     (atom? 'foobar)
-    (atom? 'match)
-    (atom? 'match)
+    (not (atom? 'match))
     (verb? 'match)
-    (verb? 'foobar)
-    (variable-pattern? '(test Foo))
+    (not (verb? 'foobar))
+    (not (variable-pattern? '(test Foo)))
     (variable-pattern? ',foo)
-    (variable-pattern? ',match)
-    ])
+    (not (variable-pattern? ',match))
+    (not (atom? '_))
+    (not (atom? '_foo))
+    (wildcard-pattern? '_)
+    (wildcard-pattern? '_foo)
+    (not (wildcard-pattern? 'foo))
+    ))
 
-(pretty-print (parse-Lsrc '(system main (actor main s) (actor aux s))))
+(pretty-print
+  (parse-Lsrc
+    '(system
+       [(start main)]
+       (define (main s)
+         [(msg1 ,x) (become empty '())]
+         [(msg2 ,z) (spawn aux 2)]
+         [_ (stop)])
+       (define (empty s))
+       (define (aux s)
+         [1 (become (actor (y) [_ (stop)]) x)]
+         ['() (stay x)]
+         [3 (send (self) 2)]
+         [_ (stay x)])
+       )))
